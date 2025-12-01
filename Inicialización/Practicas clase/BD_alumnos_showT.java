e903588fe95912dba8ce199a567ecf28bcdf665f
Marcos Almorox <marcos.almorox@gmail.com>
2025-12-01 23:41:49 +0100

    practica 2 de listar tablas terminada

---
+++ Inicialización/Practicas clase/BD_alumnos_showT.java
@@ -0,0 +1,26 @@
+package dbmysql;
+
+import java.sql.*;
+
+public class BD_alumnos_showT {
+
+    public void ListarTablas() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        System.out.println("Obteniendo conexión...");
+
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/bd_alumnos", "root", "");
+        Statement st = c.createStatement();
+        ResultSet rs = st.executeQuery("SHOW TABLES");
+
+        System.out.println("Tablas de la base de datos:");
+
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+
+        rs.close();
+        st.close();
+        c.close();
+    }
+}
--- Inicialización/Practicas clase/Practica2_ListarTablas.java
+++ Inicialización/Practicas clase/Practica2_ListarTablas.java
@@ -3,6 +3,11 @@ import java.sql.*;
 
 public class Practica2_ListarTablas {
     public static void main(String[] args) {
-
+        try {
+            BD_alumnos_showT bd = new BD_alumnos_showT();
+            bd.ListarTablas();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
